78dc504c52b6170d483f75b9947b92e153b90e12
Анастасия <folk.baskakova@yandex.ru>
2026-09-21 00:57:09 +0300

    Добавлены нужные импорты и изменен файл с конфигурациями

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import uvicorn
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 from pathlib import Path
 
 from posts.routers import router as posts_api_router
@@ -10,12 +11,13 @@ from users.routers import router as users_api_router
 from users.views import router as users_page_router
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-app.mount("/static", StaticFiles(directory=BASE_DIR / "static"), name="static")
 templates = Jinja2Templates(directory=BASE_DIR / "templates")                      
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title='Blog')
+
+app.mount("/static", StaticFiles(directory=BASE_DIR / "static"), name="static")
 
 app.include_router(posts_api_router)
 app.include_router(posts_page_router)
